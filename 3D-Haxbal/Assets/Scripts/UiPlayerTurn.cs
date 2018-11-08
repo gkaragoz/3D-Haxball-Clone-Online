34176d841108da54e6d89ef50942ec79353464c6
pickupst <gd.okanuslu@gmail.com>
2018-11-08 17:42:27 +0300

    Make Positioning game menu

---
+++ 3D-Haxbal/Assets/Scripts/UiPlayerTurn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiPlayerTurn : MonoBehaviour {
+
+	public float turnSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed);
+	}
+}
