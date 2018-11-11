3b2682f58e30f1830c7ce1e10077758f89ec2b19
Cfee <cfeelink@gmail.com>
2018-11-11 20:55:42 +0300

    UI changes
    Market panel changed
    Added character rotation at selection screen

---
+++ 3D-Haxball/Assets/Scripts/RotateModels.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModels : MonoBehaviour {
+
+    public float sensitivity = 20.0f;
+    private Transform _CameraTransform;
+
+    void Start()
+    {
+
+        _CameraTransform = Camera.main.transform;
+        
+    }
+    private void OnMouseDrag()
+    {
+        float rotationX = Input.GetAxis("Mouse X") * sensitivity;
+        transform.Rotate(_CameraTransform.up, -rotationX, Space.World);
+      
+    }
+ 
+    void Update()
+    {
+        transform.Rotate(Vector3.up*Time.deltaTime*sensitivity);
+    }
+}
