f78947405333d3dbfd787a73f758e878521f0a62
gkaragoz <gkaragoz96@gmail.com>
2018-11-13 13:10:57 +0300

    Bake gameplay scene. Add new stadium.

--- 3D-Haxball/Assets/Scripts/CameraControlller.cs
+++ 3D-Haxball/Assets/Scripts/CameraControlller.cs
@@ -17,8 +17,7 @@ public class CameraControlller : MonoBehaviour {
     #endregion
 
     public float smooth = 1.5f;
-    public float xMultiplier = -0.5f;
-    public float offset = 35f;
+    public Vector3 offset;
 
     private Transform _ballTransform;
     private Transform _playerTransform;
@@ -27,7 +26,7 @@ public class CameraControlller : MonoBehaviour {
         _ballTransform = GameObject.Find("Ball").transform;
     }
 
-    private void LateUpdate() {
+    private void FixedUpdate() {
         if (_ballTransform == null || _playerTransform == null)
             return;
 
@@ -35,19 +34,7 @@ public class CameraControlller : MonoBehaviour {
     }
 
     public void Move() {
-        Vector3 midpoint = (_ballTransform.position + _playerTransform.position) * 0.5f;
-        float distance = Vector3.Distance(_ballTransform.position, _playerTransform.position);
-
-        Vector3 desiredDestination = midpoint - transform.forward * ZoomFactorFinder(_ballTransform.transform.position.z);
-
-        desiredDestination.x = transform.position.x;
-
-        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, new Vector3(-1f * _ballTransform.transform.position.z + 40f, 0f, 0f), smooth * Time.deltaTime);
-        transform.position = Vector3.Slerp(transform.position, desiredDestination, smooth * Time.deltaTime);
-    }
-
-    public float ZoomFactorFinder(float distance) {
-        return xMultiplier * distance + offset; //ref https://keisan.casio.com/exec/system/1223508685
+        transform.position = Vector3.Slerp(transform.position, _playerTransform.position + offset, smooth * Time.deltaTime);
     }
 
     public void SetTarget(Transform playerTarget) {
